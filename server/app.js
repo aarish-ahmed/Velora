0a3faf9cd34bf1d0a7edfa41f98f1ed7fbb871e7
aarish <aarishahmed96@gmail.com>
2026-07-17 12:48:57 +0600

    Allow Netlify domain in CORS configuration

--- server/app.js
+++ server/app.js
@@ -5,12 +5,13 @@ import appRoutes from "./routes/appRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials:true,
-  })
-);
+app.use(cors({
+  origin: [
+    "http://localhost:5173", 
+    "https://velora-x.netlify.app"
+  ],
+  credentials: true
+}));
 ''
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
