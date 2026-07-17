1065ca05619024455f6c01195e611829e79a64ba
aarish <aarishahmed96@gmail.com>
2026-07-17 16:47:08 +0600

    changed send email fo ipv6

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,7 +2,11 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async (email, verificationCode) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // Required for port 587
+    requireTLS: true,
+    family: 4, // CRITICAL: Forces IPv4 routing so Render's cloud servers don't drop the connection
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
